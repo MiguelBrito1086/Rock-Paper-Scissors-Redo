c6c07d83fc5c96aebdad4181dc69ce380866da47
MiguelBrito1086 <mbrito1086@gmail.com>
2017-08-08 22:08:48 -0400

    Working through JS, image showing

--- main.js
+++ main.js
@@ -1,37 +1,81 @@
-const express = require('express')
-const mustacheExpress = require('mustache-express')
+function randomNess(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+}
 
-const app = express()
+let throws = ['rock', 'paper', 'scissors']
 
-app.engine('mst', mustacheExpress())
-app.set('view engine', 'mst')
-app.set('views', './views')
+let computerNumber = randomNess(0,2)
 
-app.use(express.static('public'))
+let computer = throws[computerNumber]
 
-app.get('/', (req, resp) => {
-  resp.render('index')
+let vs = document.querySelector('#myDraw')
+
+let rock = document.getElementById('rock')
+let paper = document.getElementById('paper')
+let scissors = document.getElementById('scissors')
+
+let me = document.addEventListener('click', image => {
+    let p = document.createElement('p')
+    let img = document.createElement('img')
+
+    if (throws[i] === 'rock') {
+      img.src = "rock.gif"
+    }
+    if (paper) {
+      img.src = "paper.png"
+    }
+    if (scissors) {
+      img.src = "scissors.png"
+    }
+
+    vs.appendChild(p)
+    p.appendChild(img)
 })
 
-app.post('/' (req, resp) => {
-  const pick = 
-})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, () => {
-  console.log("Magic is happening on port 3000")
-});
+// console.log('The computer chose', computer, 'and you chose', me)
+// //
+// // document.getElementById(computer)Math.random("rock","paper","scissors")
+// //
+// // document.getElementById(me)Math.random("rock","paper","scissors")
+//
+// // let computer = "rock";
+// // let me = "scissors"; // Change this to test your code
+//
+// if (me === "rock" && computer === "rock") {
+//   console.log("Looks like a tie!");
+// }
+//
+// if (me === "paper" && computer === "rock") {
+//   console.log("I win!");
+// }
+//
+// if (me === "scissors" && computer === "rock") {
+//   console.log("Computer wins!");
+// }
+//
+// if (me === "rock" && computer === "paper") {
+//   console.log("Computer wins!");
+// }
+//
+// if (me === "paper" && computer === "paper") {
+//   console.log("Looks like a tie!");
+// }
+//
+// if (me === "scissors" && computer === "paper") {
+//   console.log("I win!");
+// }
+//
+// if (me === "rock" && computer === "scissors") {
+//   console.log("I win!");
+// }
+//
+// if (me === "paper" && computer === "scissors") {
+//   console.log("Computer wins!");
+// }
+//
+// if (me === "scissors" && computer === "scissors") {
+//   console.log("Looks like a tie!");
+// }
