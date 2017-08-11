b5b38d352b3c5bdad36e1aa3ad72caad2c806f62
MiguelBrito1086 <mbrito1086@gmail.com>
2017-08-11 14:12:17 -0400

    Computer func and win,tie,lose

--- main.js
+++ main.js
@@ -3,116 +3,95 @@ let comp = document.querySelector('#compDraw')
 let rock = document.getElementById('rock')
 let paper = document.getElementById('paper')
 let scissors = document.getElementById('scissors')
+let result = document.getElementById('VS')
+let myPoints = document.getElementById('myPoints')
+let compPoints = document.getElementById('compPoints')
 
+let me
+let computer
+
+
+function randomNess(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 let userClick = choice => {
-  let p = document.createElement('p')
-  let img = document.createElement('img')
-
-  if (scissors) {
-    img.src = `${choice}.png`
-  }
-
-  vs.appendChild(p)
-  p.appendChild(img)
-
-  // Call function that does computer move
-  function randomNess(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-
-    let compPick = choice => {
-      let p = document.createElement('p')
+      me = choice
       let img = document.createElement('img')
 
       if (scissors) {
         img.src = `${choice}.png`
       }
 
-      comp.appendChild(p)
-      p.appendChild(img)
+      vs.appendChild(img)
+
+      compPick();
+      results();
+      total();
+      }
+
+let compPick = () => {
+      let img = document.createElement('img')
+
+      comp.appendChild(img)
+
 
       let throws = ['rock', 'paper', 'scissors']
 
       let computerNumber = randomNess(0,2)
 
-      let computer = throws[computerNumber]
-      compPick()
-    }
-  }
+      computer = throws[computerNumber]
 
+      img.src = `${computer}.png`
+    }
+
+
+scissors.addEventListener('click', image => {
+  userClick('scissors')
+})
+rock.addEventListener('click', image => {
+  userClick('rock')
+})
+paper.addEventListener('click', image => {
+  userClick('paper')
+})
+
+
+
+let results = () => {
+
+      if ( (me === "rock" && computer === "rock") || (me === "paper" && computer === "paper") || (me === "scissors" && computer === "scissors") ) {
+        setTimeout(() => {
+          result.textContent = "Tie Game!!";
+          result.className += "tie";
+        }, 500);
+      }
+
+      if ( (me === "paper" && computer === "rock") || (me === "rock" && computer === "scissors") || (me === "scissors" && computer === "paper") ) {
+        setTimeout(() => {
+          result.textContent = "You Win!!";
+          result.className += "win";
+        }, 500);
+      }
+
+      if ( (me === "scissors" && computer === "rock") || (me === "rock" && computer === "paper") || (me === "paper" && computer === "scissors") ) {
+        setTimeout(() => {
+          result.textContent = "Computer Wins!!";
+          result.className += "lose";
+        }, 500);
+      }
 }
 
-
-
-let me =
-    scissors.addEventListener('click', image => {
-      userClick('scissors')
-    })
-    rock.addEventListener('click', image => {
-      userClick('rock')
-    })
-    paper.addEventListener('click', image => {
-      userClick('paper')
-    })
-
-
-    if ( (me === "rock" && computer === "rock") || (me === "paper" && computer === "paper") || (me === "scissors" && computer === "scissors") ) {
-    // Tie Game
-
-    }
-
-    if ( (me === "paper" && computer === "rock") || (me === "rock" && computer === "scissors") || (me === "scissors" && computer === "paper") ) {
-    // I win
-
-    }
-
-    if ( (me === "scissors" && computer === "rock") || (me === "rock" && computer === "paper") || (me === "paper" && computer === "scissors") ) {
-    // computer wins
-
-    }
+// let total = () => {
 //
-// document.getElementById(computer)Math.random("rock","paper","scissors")
+//     // let myPoints = 0;
+//     // let compPoints = 0;
 //
-// document.getElementById(me)Math.random("rock","paper","scissors")
-
-// let computer = "rock";
-// let me = "scissors"; // Change this to test your code
-// let play = me.addEventListener
-//
-//     if (me === "rock" && computer === "rock") {
-//       console.log("Looks like a tie!");
+//     if (result.textContent = 'win') {
+//       myPoints.textContent += 1
+//     } else {
+//       compPoints.textContent += 1
 //     }
-//
-//     if (me === "paper" && computer === "rock") {
-//       console.log("I win!");
-//     }
-//
-//     if (me === "scissors" && computer === "rock") {
-//       console.log("Computer wins!");
-//     }
-//
-//     if (me === "rock" && computer === "paper") {
-//       console.log("Computer wins!");
-//     }
-//
-//     if (me === "paper" && computer === "paper") {
-//       console.log("Looks like a tie!");
-//     }
-//
-//     if (me === "scissors" && computer === "paper") {
-//       console.log("I win!");
-//     }
-//
-//     if (me === "rock" && computer === "scissors") {
-//       console.log("I win!");
-//     }
-//
-//     if (me === "paper" && computer === "scissors") {
-//       console.log("Computer wins!");
-//     }
-//
-//     if (me === "scissors" && computer === "scissors") {
-//       console.log("Looks like a tie!");
-//     }
+// }
